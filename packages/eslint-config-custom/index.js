c741d1d594e81bf4bddc83771634dd0981e19c82
aburto22 <aburto22@gmail.com>
2022-12-22 14:18:13 +0100

    fixed eslint and ts monorepo

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "turbo", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "turbo",
+    "prettier",
+  ],
   rules: {},
+  ignorePatterns: ["node_modules", "dist", ".eslintrc.js"],
 };
