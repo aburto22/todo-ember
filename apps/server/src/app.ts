35c3bbbae8a3d1a0beaf92604bae3c1cdb3253d7
aburto22 <aburto22@gmail.com>
2022-12-23 10:43:51 +0100

    working monorepo, tests not working though

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get('/', (req, res) => {
+  res.send('hello world');
 });
 
 export default app;
